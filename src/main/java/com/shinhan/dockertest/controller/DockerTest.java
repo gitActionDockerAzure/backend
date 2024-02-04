845051eb9fd21737ba94f72e90cc6d3b8d118e63
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-02-05 04:22:24 +0900

    Update DockerTest.java

--- src/main/java/com/shinhan/dockertest/controller/DockerTest.java
+++ src/main/java/com/shinhan/dockertest/controller/DockerTest.java
@@ -1,11 +1,13 @@
 package com.shinhan.dockertest.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerTest {
 	
+	@CrossOrigin(origins = "http://localhost:3000")
 	@GetMapping("/test")
 	public String f1() {
 		return "dockerTestConnectionSuccess";
