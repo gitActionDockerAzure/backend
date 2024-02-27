5e50d8dd25e1ac79126f76dfa8df54d0955e293e
Hwasoo-Jeon <134925813+Hwasoo-Jeon@users.noreply.github.com>
2024-02-27 15:53:24 +0900

    Update DockerTest.java

--- src/main/java/com/shinhan/dockertest/controller/DockerTest.java
+++ src/main/java/com/shinhan/dockertest/controller/DockerTest.java
@@ -7,9 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerTest {
 	
-	@CrossOrigin(origins = "https://cardvisor.azurewebsites.net")
-	@GetMapping("/api/test")
+	@GetMapping("/test")
 	public String f1() {
 		return "dockerTestConnectionSuccess";
+	}
+
+	@GetMapping("/temp")
+	public String temp() {
+		return "temp 스프링 접근 완료";
+	}
+	@GetMapping("/tt")
+	public String temp2() {
+		return "tt 스프링 접근 완료";
 	}
 }
